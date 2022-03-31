83a60367ca4be74feca8073cc4ffd101f51ffb52
Marlon Clay <luciousmc@icloud.com>
2022-03-31 15:22:26 -0700

    feat: add grid columns to main

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,7 +1,7 @@
 function NFTDopPage() {
   return (
-    <main className="flex h-screen flex-col">
-      <section className="bg-gradient-to-br from-cyan-800 to-rose-500">
+    <main className="flex h-screen flex-col lg:grid lg:grid-cols-10">
+      <section className="bg-gradient-to-br from-cyan-800 to-rose-500 lg:col-span-4">
         <div className="flex flex-col items-center justify-center py-2 lg:min-h-screen">
           <div className="rounded-xl bg-gradient-to-br from-yellow-400 to-purple-600 p-2">
             <img
@@ -10,7 +10,7 @@ function NFTDopPage() {
               alt="Ape Image"
             />
           </div>
-          <div>
+          <div className="space-y-2 p-5 text-center">
             <h1 className="text-4xl font-bold text-white">Monahs Apes</h1>
             <h2 className="text-xl text-gray-300">A collection of apes</h2>
           </div>
