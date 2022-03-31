6c596389d5da0a37ebabc36a52b3d6ede901f649
Marlon Clay <luciousmc@icloud.com>
2022-03-31 15:16:52 -0700

    feat: add dynamic route for collections

---
+++ pages/nft/[id].tsx
@@ -0,0 +1,24 @@
+function NFTDopPage() {
+  return (
+    <main className="flex h-screen flex-col">
+      <section className="bg-gradient-to-br from-cyan-800 to-rose-500">
+        <div className="flex flex-col items-center justify-center py-2 lg:min-h-screen">
+          <div className="rounded-xl bg-gradient-to-br from-yellow-400 to-purple-600 p-2">
+            <img
+              className="w-44 rounded-xl object-cover lg:h-96 lg:w-72"
+              src="https://links.papareact.com/8sg"
+              alt="Ape Image"
+            />
+          </div>
+          <div>
+            <h1 className="text-4xl font-bold text-white">Monahs Apes</h1>
+            <h2 className="text-xl text-gray-300">A collection of apes</h2>
+          </div>
+        </div>
+      </section>
+
+      {/* Right Side */}
+    </main>
+  );
+}
+export default NFTDopPage;
